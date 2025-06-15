1ee98d1406b7c573b839b7fe8ce3a5cd9b0c687a
Dalton Cabrera <daltoncabrera@gmail.com>
2025-06-15 01:46:34 -0400

    launch url env var

--- MSESG.CargoCare.Web/Program.cs
+++ MSESG.CargoCare.Web/Program.cs
@@ -13,7 +13,7 @@ namespace MSESG.CargoCare.Web
     public class Program
     {
         public static IConfiguration Configuration { get; set; }
-        private static int _port = 8090;
+        
         public static void Main(string[] args)
         {
             var builder = new ConfigurationBuilder()
@@ -23,25 +23,11 @@ namespace MSESG.CargoCare.Web
 
             Configuration = builder.Build();
 
-            // Read port from environment variable first, then from configuration
-            var portFromEnv = Environment.GetEnvironmentVariable("PORT");
-            if (!string.IsNullOrWhiteSpace(portFromEnv) && int.TryParse(portFromEnv, out int envPort))
-            {
-                _port = envPort;
-            }
-            else if (!string.IsNullOrWhiteSpace(Configuration["port"]))
-            {
-                _port = Convert.ToInt32(Configuration["port"]);
-            }
-
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args).UseKestrel(o =>
-                {
-                    o.Listen(IPAddress.Loopback, _port);
-                })
+            WebHost.CreateDefaultBuilder(args)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseSetting("detailedErrors", "true")
                 .UseIISIntegration()
